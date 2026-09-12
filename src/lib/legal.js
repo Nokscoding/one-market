9204c3736c06d56efe63a62ea2cd93099850cdc1
Nokscoding <hckh265@gmail.com>
2026-09-12 16:04:21 +0200

    feat: add One Market legal consent constants

---
+++ src/lib/legal.js
@@ -0,0 +1,8 @@
+export const LEGAL_VERSION = '2026-09-12'
+
+export const LEGAL_LINKS = {
+  terms: '/legal/conditions',
+  privacy: '/legal/confidentialite',
+  cookies: '/legal/cookies',
+  seller: '/legal/vendeurs',
+}
